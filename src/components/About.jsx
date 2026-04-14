541bc29820ac6aba2cadfa4507afa5bc95caa4e5
Aryan2521-04 <Aryaman_Shah@student.uml.edu>
2026-04-14 11:12:46 -0400

    Updated Resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -46,7 +46,7 @@ export default function About() {
     },
     {
       label: "Resume",
-      href: "/Aryaman_shah-5.pdf",
+      href: "/Aryaman_shah-6.pdf",
       icon: FaFileAlt,
       style: "bg-blue-600 text-white hover:bg-blue-700",
       download: true,
